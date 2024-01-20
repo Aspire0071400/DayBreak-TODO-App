1d4c472a38f8917bd183b63e9b8a2fdcfdc3d402
Aspire0071400 <prabhujivats@gmail.com>
2024-01-20 17:16:32 +0530

    Update_task_Dialog completed UI

--- app/src/main/java/com/aspire/daybreaktodo_app/MainActivity.kt
+++ app/src/main/java/com/aspire/daybreaktodo_app/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.aspire.daybreaktodo_app
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.aspire.daybreaktodo_app.databinding.ActivityMainBinding
+import com.aspire.daybreaktodo_app.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+    private val mainBinding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val addTaskDialog : Dialog by lazy {
+        Dialog(this).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+    private val updateTaskDialog : Dialog by lazy {
+        Dialog(this).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+        val close_btn_addTaskDialog = addTaskDialog.findViewById<ImageView>(R.id.close_btn_addTaskDialog)
+        val update_btn_updateTaskDialog = updateTaskDialog.findViewById<ImageView>(R.id.close_btn_updateTaskDialog)
+
+        close_btn_addTaskDialog.setOnClickListener{addTaskDialog.dismiss()}
+        update_btn_updateTaskDialog.setOnClickListener { updateTaskDialog.dismiss()}
+
+        mainBinding.addTaskFAB.setOnClickListener {
+            addTaskDialog.show()
+        }
     }
 }
